c3691d1c98b6c8e1e50c2cf1f78096c532e8ba3e
Pedro Silva <pedro@acryl.io>
2024-05-09 10:26:54 +0100

    feat(cli): Make consistent use of DataHubGraphClientConfig

--- metadata-ingestion/src/datahub/ingestion/graph/client.py
+++ metadata-ingestion/src/datahub/ingestion/graph/client.py
@@ -25,7 +25,7 @@ from avro.schema import RecordSchema
 from deprecated import deprecated
 from requests.models import HTTPError
 
-from datahub.cli.cli_utils import get_url_and_token
+from datahub.cli.cli_utils import load_graph_config
 from datahub.configuration.common import ConfigModel, GraphError, OperationalError
 from datahub.emitter.aspect import TIMESERIES_ASPECT_MAP
 from datahub.emitter.mce_builder import DEFAULT_ENV, Aspect
@@ -1205,7 +1205,7 @@ class DataHubGraph(DatahubRestEmitter):
 
 
 def get_default_graph() -> DataHubGraph:
-    (url, token) = get_url_and_token()
-    graph = DataHubGraph(DatahubClientConfig(server=url, token=token))
+    graph_config = load_graph_config()
+    graph = DataHubGraph(graph_config)
     graph.test_connection()
     return graph
